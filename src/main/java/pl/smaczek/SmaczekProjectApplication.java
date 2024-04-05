5e236cdee552b81c305686dc30d8d53fb83289ae
LukaszSz90 <lukasz90sz@gmail.com>
2024-04-05 13:46:14 +0200

    Initialize structure of project
    Add empty entity classes needed in the project

---
+++ src/main/java/pl/smaczek/SmaczekProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.smaczek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmaczekProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmaczekProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/smaczek/SmaczekProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.smaczek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmaczekProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
